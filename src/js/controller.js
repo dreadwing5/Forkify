7e6048b706ad085f7a8aea52b7706b729d420ee5
dreadwing5 <sachinspyk@outlook.com>
2021-08-23 19:40:37 +0530

    Refactoring for MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,9 @@
-import {
-  getRecipeHtml,
-  getSpinnerHtml,
-  renderRecipe,
-  renderSpinner,
-} from "./template";
+import "regenerator-runtime/runtime";
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
 
+// import "core-js/stable";
 //Polyfill
-/* import "core-js/stable";
-import "regenerator-runtime/runtime"; */
 
 const recipeContainer = document.querySelector(".recipe");
 
@@ -19,42 +15,26 @@ const timeout = function (s) {
   });
 };
 
-const showRecipe = async function () {
+const controlRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     //1). Loading Recipe
-    const spinnerHtml = getSpinnerHtml();
-    recipeContainer.insertAdjacentHTML("afterbegin", spinnerHtml);
 
-    const res = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc7e"
-    );
-    const data = await res.json();
+    recipeView.renderSpinner();
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
+    await model.loadRecipe(id);
 
     //2).Rendering Recipe
-    recipeContainer.innerHTML = "";
-    const recipeHtml = getRecipeHtml(recipe);
 
-    recipeContainer.insertAdjacentHTML("afterbegin", recipeHtml);
-
-    console.log(recipe);
+    recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
   }
 };
 
-showRecipe();
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controlRecipe)
+);
