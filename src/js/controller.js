79b3a2d8b1feba37c3731626ade4f195d9de9cd8
dreadwing5 <sachinspyk@outlook.com>
2021-08-26 17:04:56 +0530

    Improved bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
+import bookmarksView from "./views/bookmarksView.js";
 
 const controlRecipe = async function () {
   try {
@@ -15,6 +16,8 @@ const controlRecipe = async function () {
 
     //0 ). Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    bookmarksView.update(model.state.bookmarks);
     //1). Loading Recipe
 
     await model.loadRecipe(id);
@@ -66,13 +69,17 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  //Add or remove bookmark
   if (!model.state.recipe.bookmarked) {
     model.addBookmark(model.state.recipe);
   } else {
     model.deleteBookmark(model.state.recipe.id);
   }
-  console.log(model.state.recipe);
+  //Update recipe view
   recipeView.update(model.state.recipe);
+
+  //Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
