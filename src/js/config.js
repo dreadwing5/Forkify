2f43b43875344df1a35b3119a02c99c548c35b29
dreadwing5 <sachinspyk@outlook.com>
2021-08-27 01:53:53 +0530

    Fixed recipe bug

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = "6f6b918c-25e2-4239-a438-932cde7b8005";
+export const MODAL_CLOSE_SEC = 2.5;
